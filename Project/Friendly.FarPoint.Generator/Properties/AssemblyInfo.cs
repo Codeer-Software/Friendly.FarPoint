2577f6b74d9b0f6a42d80e289d8634814cd74406
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2023-07-07 13:02:34 +0900

    version up.

--- Project/Friendly.FarPoint.Generator/Properties/AssemblyInfo.cs
+++ Project/Friendly.FarPoint.Generator/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.21.0")]
-[assembly: AssemblyFileVersion("1.5.21.0")]
+[assembly: AssemblyVersion("1.5.22.0")]
+[assembly: AssemblyFileVersion("1.5.22.0")]
